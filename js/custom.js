f1d294a8c50492f9760af377611a70b2ffbc9fb1
DVS JAGAN MOHAN RAO <dvsjmr9@gmail.com>
2020-10-10 12:31:20 +0530

    reinitialized files

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+/* 
+
+1. Add your custom JavaScript code below
+2. Place the this code in your template:
+
+  
+
+*/
+// $(document).ready(function() {
+//     $("#header").load("header.html");
+//     $("#footer").load("footer.html");
+// });
